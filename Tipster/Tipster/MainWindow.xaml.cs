febe69af2794ca9d14b4441391ef56c3faa7b84d
Irene Smith <irenesmith13@gmail.com>
2019-10-29 23:31:14 -0400

    Updating the Tipster app according to code analysis suggestions.

--- Tipster/Tipster/MainWindow.xaml.cs
+++ Tipster/Tipster/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Tipster
 {
@@ -27,51 +16,50 @@ namespace Tipster
 
         private void cmdCalculate_Click(object sender, RoutedEventArgs e)
         {
-            if (txtNumDiners.Text == string.Empty)
-                txtNumDiners.Text = "1";
-            int numDiners;
-            if (!int.TryParse(txtNumDiners.Text, out numDiners))
-                numDiners = 1;
-            double tipPercent;
-            if(double.TryParse(txtTipPercent.Text, out tipPercent))
+
+            if (string.IsNullOrEmpty(txtNumDiners.Text))
+                txtNumDiners.Text = Properties.Resources.MIN_DINERS_STR;
+
+            if (!int.TryParse(txtNumDiners.Text, out int numDiners))
+                numDiners = Properties.Settings.Default.MIN_DINERS;
+
+            if(double.TryParse(txtTipPercent.Text, out double tipPercent))
             {
                 if (tipPercent < 0)
                     tipPercent = 0;
                 else if (tipPercent > 100)
                     tipPercent = 100;
-                txtTipPercent.Text = tipPercent.ToString();
+                txtTipPercent.Text = tipPercent.ToString(new CultureInfo("en-US"));
                 tipPercent /= 100;
             }
             else
             {
-                tipPercent = 0;
-                txtTipPercent.Focus();
-                lblError.Text = "Tip percent must be a number between 0 and 100.";
+                _ = txtTipPercent.Focus();
+                lblError.Text = Properties.Resources.ERR_PERCENT_OUT_OF_RANGE;
                 return;
             }
 
-            double mealTotal;
-            if(!double.TryParse(txtMeal.Text, out mealTotal))
+            if(!double.TryParse(txtMeal.Text, out double mealTotal))
             {
-                lblError.Text = "The cost of the meal must be a positive decimal number.";
-                txtMeal.Focus();
+                lblError.Text = Properties.Resources.ERR_NEGATIVE_COST;
+                _ = txtMeal.Focus();
                 return;
             }
             else
             {
                 if (mealTotal < 0)
                 {
-                    lblError.Text = "The cost of the meal must be a positive decimal number.";
-                    txtMeal.Focus();
+                    lblError.Text = Properties.Resources.ERR_NEGATIVE_COST;
+                    _ = txtMeal.Focus();
                     return;
                 }
 
                 double tipAmount = mealTotal * tipPercent;
                 double totalCost = mealTotal + tipAmount;
                 double dinerAmount = totalCost / numDiners;
-                txtTipAmount.Text = string.Format("{0:C2}", tipAmount);
-                txtTotalBill.Text = string.Format("{0:C2}", totalCost);
-                txtPricePerDiner.Text = string.Format("{0:C2}", dinerAmount);
+                txtTipAmount.Text = string.Format(new CultureInfo("en-US"), "{0:C2}", tipAmount);
+                txtTotalBill.Text = string.Format(new CultureInfo("en-US"), "{0:C2}", totalCost);
+                txtPricePerDiner.Text = string.Format(new CultureInfo("en-US"), "{0:C2}", dinerAmount);
                 lblError.Text = "";
             }
         }
@@ -79,13 +67,13 @@ namespace Tipster
         private void cmdReset_Click(object sender, RoutedEventArgs e)
         {
             txtMeal.Text = "";
-            txtTipPercent.Text = "10";
-            txtNumDiners.Text = "1";
+            txtTipPercent.Text = Properties.Resources.DEFAULT_TIP_STR;
+            txtNumDiners.Text = Properties.Resources.MIN_DINERS_STR;
             txtTipAmount.Text = "";
             txtTotalBill.Text = "";
             txtPricePerDiner.Text = "";
             lblError.Text = "";
-            txtMeal.Focus();
+            _ = txtMeal.Focus();
         }
 
         private void OnTextBoxFocus(object sender, RoutedEventArgs e)
--- Tipster/Tipster/Properties/AssemblyInfo.cs
+++ Tipster/Tipster/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Tipster")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A small utility to calculate the tip and cost per diner.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Hewlett-Packard")]
 [assembly: AssemblyProduct("Tipster")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2015")]
+[assembly: AssemblyCopyright("Copyright © Irene P. Smith 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
